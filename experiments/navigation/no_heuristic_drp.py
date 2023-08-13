1ad76db412331411c86121ac7e2d76452f9587dc
Daniel Dias <danielbpdias@gmail.com>
2023-08-13 20:59:09 -0300

    Updating DRP experiments

--- experiments/navigation/no_heuristic_drp.py
+++ experiments/navigation/no_heuristic_drp.py
@@ -1,12 +1,9 @@
-
-import jax
-import optax
 import os
 
 from pyRDDLGym import RDDLEnv
 from pyRDDLGym.Core.Jax.JaxRDDLBackpropPlanner import JaxDeepReactivePolicy
 
-from _utils import run_experiment, run_planner, save_data
+from _utils import run_experiment, run_planner, save_data, save_time
 from _common_params import get_planner_params, NETWORK_TOPOLOGY
 
 root_folder = os.path.dirname(__file__)
@@ -18,6 +15,7 @@ probabilistic_environment = RDDLEnv.RDDLEnv(domain=probabilistic_domain_file, in
 
 probabilistic_planner_parameters = get_planner_params(plan=JaxDeepReactivePolicy(topology=NETWORK_TOPOLOGY), drp=True)
 
-probabilistic_final_policy_weights, probabilistic_statistics_history = run_experiment("Probabilistic (no heuristic) - DRP", run_planner, environment=probabilistic_environment, planner_parameters=probabilistic_planner_parameters)
+probabilistic_final_policy_weights, probabilistic_statistics_history, experiment_time = run_experiment("Probabilistic (no heuristic) - DRP", run_planner, environment=probabilistic_environment, planner_parameters=probabilistic_planner_parameters)
 save_data(probabilistic_final_policy_weights, f'{root_folder}/zzz_probabilistic_no_heuristic_deepreactive_policy.pickle')
 save_data(probabilistic_statistics_history, f'{root_folder}/zzz_probabilistic_no_heuristic_deepreactive_statistics.pickle')
+save_time("Probabilistic (no heuristic) - DRP", experiment_time, f'{root_folder}/zzz_deepreactive_time.csv')
