9c7c114d1bf09c62103da705bdc290e7389758f3
Daniel Dias <danielbpdias@gmail.com>
2023-08-16 23:20:33 -0300

    Update experiment runner

--- experiments/_run_all_experiments.sh
+++ experiments/_run_all_experiments.sh
@@ -4,13 +4,8 @@ echo "Running all experiments"
 echo "--------------------------------------------------------------------------------"
 echo ""
 
-start=$(date +%s)
-
-sh ./_run_drp.sh reservoir
-sh ./_run_drp.sh hvac
-sh ./_run_straightline.sh reservoir
-sh ./_run_straightline.sh hvac
-sh ./_run_straightline.sh navigation
-
-end=$(date +%s)
-echo "Elapsed Time: $(($end-$start)) seconds"
+sh ./experiments/_run_straightline.sh navigation
+sh ./experiments/_run_straightline.sh hvac
+sh ./experiments/_run_drp.sh hvac
+# sh ./experiments/_run_straightline.sh reservoir
+# sh ./experiments/_run_drp.sh reservoir
